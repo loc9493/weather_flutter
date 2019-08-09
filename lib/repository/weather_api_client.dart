a76d912c78775a895dfa255c5801e0574ca3068b
Loc Nguyen <loc.nguyen@saleshood.com>
2019-08-09 08:04:24 +0700

    finish

--- lib/repository/weather_api_client.dart
+++ lib/repository/weather_api_client.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 class WeatherApiClient {
   static final String baseURL = "https://api.openweathermap.org/";
   final http.Client httpClient;
-  final APP_TOKEN = "2b99151af6edd2eac0b2eceebeddd35e";
+  final APP_TOKEN = "Token"; // Replace your token here
   WeatherApiClient({@required this.httpClient}) : assert(httpClient != null);
   Future<CityWeather> getCityWeather(String cityID) async {
     final url = baseURL + "/data/2.5/weather?id=$cityID&APPID=$APP_TOKEN";
